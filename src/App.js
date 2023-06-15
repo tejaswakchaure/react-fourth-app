c45c05627f2adf7fdd08f1233dadf4ddc7312520
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-15 16:34:20 +0530

    complex jsx in map function.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
-import {useRef , useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
   return (
     <>
-      <h1>Props Demo</h1>
+      <h1>Stateful List</h1>
       <ListDemo />
-
     </>
   );
-
 }
 
-
 function ListDemo() {
-  // useRef is  hook command
   let inputRef = useRef();
   let [list, setList] = useState(["delhi"]);
 
   let addItemAction = () => {
+    // console.log(inputRef.current);
+    // console.log(document.querySelector("#id1"));
 
-    // for DOM opration React use useref method for taking or finding tag
-    // let inputRef = document.querySelector("#id1");
     let inputValue = inputRef.current.value;
+    let newList = [inputValue, ...list];
+    setList(newList);
 
-    let updateList = [inputValue, ...list, "mumbai"];
-
-    // DOM :: TRIGGER DOM
-    setList(updateList);
-
-    inputRef.value = "";
+    inputRef.current.value = "";
   };
 
   return (
     <>
-      <input type="text" ref={inputRef} placeholder="Enter user input........"/>
-      <input type="button" value="Add new Item" onClick={addItemAction} />
-      <hr />
+      <input
+        type="text"
+        id="id1"
+        ref={inputRef}
+        placeholder="Enter user input..."
+      />
+      <input type="button" value="Add New Item" onClick={addItemAction} />
 
       {list.map((item) => (
-        <h1>{item}</h1>))}
+
+        <div>
+          <h1>{item}</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+            sit laborum quia non hic doloribus id quisquam consequuntur quis
+            iusto quod, assumenda vitae, molestias ipsa ex labore quaerat
+            ducimus accusamus.
+          </p>
+          <div>
+            <input type="button" value="&#128077;" />
+            <input type="button" value="&#128078;" />
+          </div>
+        </div>
+
+
+      ))}
     </>
   );
 }
