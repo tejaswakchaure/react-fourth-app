376f5ece63174b5199ee8bc5a0b3e4069c23b7c2
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-15 09:30:55 +0530

    function with parameter

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ function App() {
     <>
     {/* calling message dema */}
       <h1>Props Demo</h1>
-      <MessageDemo />
+      {/* this id=hi these are the atribute */}
+      {/* attribute name is any valid name */}
+      <MessageDemo/>
+      <MessageDemo message="Hello"/>
+      <MessageDemo username="tejas"/>
+      <MessageDemo username="CDAC"/>
+      <MessageDemo name="Tejas Wakchaure"/>
+      {/* <MessageDemo message="hi" username:"tejas"/> */}
     </>
   );
 
 }
 //Reusable + O/p is static
-function MessageDemo() {
+//how to make dynamic..........??
+function MessageDemo(Props) {
+  console.log ("I am inside massageDemo",Props);
   return (
     <>
-      <h1>Hello Message</h1>
+      <h1>Hello {Props.username}</h1>
+      <h1>Hello {Props.name}</h1>
     </>
   )
 }
