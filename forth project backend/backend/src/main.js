6a4f6256586104e963dadaf1122e58fb5db5650b
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-15 22:11:54 +0530

    backend Connection

---
+++ forth project backend/backend/src/main.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+
+async function addTodo(req ,res){
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+
+    const db = client.db("TODO");
+    const myCollection = db.collection("todoInfo");
+
+    let todoData = {task : req.query.task ,
+                   description:req.query.description
+    }
+    await myCollection.insertOne({todoData});
+
+    await client.close()
+    res.json({status : "Add succesfully"});
+}
+
+
+
+
+app.get("/add",addTodo);
+
+
+app.listen(4000);
--- src/App.js
+++ src/App.js
@@ -1,73 +1,47 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 function App() {
   return (
     <>
-      <h1>Stateful List</h1>
-      <ListDemo />
+      <h1>Hello</h1>
+      <MyTodo />
     </>
+
   );
 }
 
-function ListDemo() {
-  let inputRef = useRef();
-  let [list, setList] = useState(["I am best"]);
-  let[counter,setCounter] = useState(0);
-  let[counter2,setCounter2] = useState(0);
+function MyTodo() {
 
-  let addItemAction = () => {
-    // console.log(inputRef.current);
-    // console.log(document.querySelector("#id1"));
-
-    let inputValue = inputRef.current.value;
-    let newList = [inputValue, ...list];
-    setList(newList);
-
-    inputRef.current.value = "";
+  let [todo,setTodo] = useState({task:"",description : ""});
+  
+  let changeTaskAction = (e) => {
+    let newTodo = {...todo,task :e.target.value};
+    setTodo(newTodo);
   };
+   
+  let changedescriptionAction = (e) => {
+    let newdescription = {...todo,description :e.target.value};
+    setTodo(newdescription);
+  }
 
-  let Likeme = () =>{
-    counter = counter +1;
-    setCounter(counter);
-    
-  };
-  let disLikeMe = () =>{
-    counter2 = counter2 +1;
-    setCounter2(counter2);
-    
-  };
+  let addTodoAction = async() => {
+    //console.log(todo);
+
+    let uri = `http://127.0.0.1:4000/add?task=${todo.task}&description=${todo.description}`;
+    await fetch(uri);
+  }
+
   return (
     <>
-      <input
-        type="text"
-        id="id1"
-        ref={inputRef}
-        placeholder="Enter user input..."
-      />
-      <input type="button" value="Add New Item" onClick={addItemAction} />
-
-      {list.map((item) => (
-
-        <div>
-          <h1>{item}</h1>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
-            sit laborum quia non hic doloribus id quisquam consequuntur quis
-            iusto quod, assumenda vitae, molestias ipsa ex labore quaerat
-            ducimus accusamus.
-          </p>
-          <div>
-            {counter}&nbsp;<input type="button" value="&#128077;" onClick={Likeme} />
-            &nbsp;&nbsp;&nbsp;
-            {counter2}&nbsp;<input type="button" value="&#128078;" onClick={disLikeMe}/>
-            <hr/>
-          </div>
-        </div>
+    <input  className="form-control" type="text" placeholder="enter todo here......" value={todo.task}  onChange={changeTaskAction}/>
 
 
-      ))}
+    <textarea  className="form-control"  cols="30" rows=" 3 " placeholder="Enter Discription" value={todo.discription} onChange={changedescriptionAction}></textarea>
+
+
+    <input  className="form-control" type="button" value="Add Todo" onClick={addTodoAction} />
     </>
+    
   );
 }
 
