4e75facd107e63bac1bc8b59ce20e1155eb8c890
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-15 10:01:45 +0530

    loop

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
 function App() {
   return (
     <>
-    {/* calling message dema */}
+      {/* calling message dema */}
       <h1>Props Demo</h1>
-      {/* this id=hi these are the atribute */}
-      {/* attribute name is any valid name */}
-      {/* user cann pass multiple paramete */}
-      <MessageDemo/>
-      <MessageDemo message="Hello"/>
-      <MessageDemo username="tejas" email="tejas@gmail.com" mobileNo="9284671730"/>
-      <MessageDemo username="CDAC" email="tejas@gmail.com" />
-      <MessageDemo name="Tejas Wakchaure"  />
-      {/* <MessageDemo message="hi" username:"tejas"/> */}
+      <ListDemo />
+
     </>
   );
 
 }
-//Reusable + O/p is static
-//how to make dynamic..........??
-//props :: destructure
-//accepting unorder
-function MessageDemo({username,email}) {
-  console.log ("I am inside massageDemo");
+
+// function ListDemo() {
+//   return (
+//     <>
+//       {/* this id=hi these are the atribute */}
+//       {/* attribute name is any valid name */}
+//       {/* user cann pass multiple paramete */}
+//       <MessageDemo />
+//       <MessageDemo message="Hello" />
+//       <MessageDemo username="tejas" email="tejas@gmail.com" mobileNo="9284671730" />
+//       <MessageDemo username="CDAC" email="tejas@gmail.com" />
+//       <MessageDemo name="Tejas Wakchaure" />
+//       {/* <MessageDemo message="hi" username:"tejas"/> */}
+
+//     </>
+//   )
+// }
+
+function ListDemo() {
+  let data = "Hello Brother";
+  let list = [ "delhi ", "mumbai" , "nashik"];
+
+  // we can put value in empty array
+  //let list = [];
+  // for( let i=0;i<10;i++ ){
+  //     list.push("Hello Tejas");
+  // }
+  console.log(list);
+
   return (
+
     <>
-      <h1>Hello {username} {email}</h1>
-      
+    {/* for loop */}
+
+      <h1> {data} </h1>
+      {/* forEach is consumer function ==> doesnt return any thing  */}
+      {list.forEach((item) => item)}
+
+      {/* Geting output of this map */}
+      {/* map is take input and give output */}
+      {list.map((item) => item)}
     </>
-  )
+  );
 }
 
 export default App;
