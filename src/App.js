3826f34fddd63fa20066aa6f3d1fcdef6a98a4fe
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-16 11:26:03 +0530

    mpa - setUp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { useState } from "react";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyRegisration from "./pages/MyRegisration";
 
 function App() {
   return (
     <>
-      <h1>Registration page</h1>
-      <MyRegisration />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>}/>
+        <Route path="/home" element={<h1>Home</h1>}/>
+        <Route path="/registration" element={<MyRegisration/>} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
-
-
 export default App;
