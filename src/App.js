9aef861cc7c8490fca395e31392fb2d50a80ec64
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-16 11:34:02 +0530

    basic navigation link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import MyRegisration from "./pages/MyRegisration";
 
@@ -6,17 +5,23 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <MyNavigationLink />
+        <Routes>
+          <Route path="/" element={<h1>Hello</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/registration" element={<MyRegisration />} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+function MyNavigationLink() {
+  return (
+    <>
       <Link to={"/home"}>Home</Link>
       <Link to={"/registration"}>registration</Link>
-
-
-      <Routes>
-        <Route path="/" element={<h1>Hello</h1>}/>
-        <Route path="/home" element={<h1>Home</h1>}/>
-        <Route path="/registration" element={<MyRegisration/>} />
-        <Route path="*" element={<h1>Page Not Found</h1>} />
-      </Routes>
-      </BrowserRouter>
     </>
   );
 }
